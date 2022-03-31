8e9b97479a6a620426a9d4b54bd4925e2773e347
jeho0131 <64771576+jeho0131@users.noreply.github.com>
2022-03-31 18:55:45 +0900

    Add files via upload

---
+++ jehoPMG/movec.py
@@ -0,0 +1,36 @@
+import tkinter
+
+key = ""
+def key_down(e):
+    global key
+    key = e.keysym
+def key_up(e):
+    global key
+    key = ""
+
+cx = 400
+cy = 300
+
+def main_proc():
+    global cx, cy
+    if key == "Up":
+        cy = cy - 10
+    if key == "Down":
+        cy = cy + 10
+    if key == "Left":
+        cx = cx - 10
+    if key == "Right":
+        cx = cx + 10
+    canvas.coords("MYCHR", cx, cy)
+    root.after(10, main_proc)
+
+root = tkinter.Tk()
+root.title("캐릭터 이동")
+root.bind("<KeyPress>", key_down)
+root.bind("<KeyRelease>", key_up)
+canvas = tkinter.Canvas(width=800, height=600, bg="lightgreen")
+canvas.pack()
+img = tkinter.PhotoImage(file="mimi.png")
+canvas.create_image(cx,cy,image=img, tag="MYCHR")
+main_proc()
+root.mainloop()
